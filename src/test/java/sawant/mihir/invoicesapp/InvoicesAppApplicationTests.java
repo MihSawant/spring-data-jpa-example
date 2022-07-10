10ca14d1dbd6b19c66aa4d1d9ae03743e7d3aaa1
Mihir Sawant <mih3190Sawant@outlook.com>
2022-07-10 12:15:46 +0530

    project structure

---
+++ src/test/java/sawant/mihir/invoicesapp/InvoicesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sawant.mihir.invoicesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
